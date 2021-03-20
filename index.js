7eba0b88f90c188265fc869f1f3eb43fb4e419d0
gayratyuldoshev-go <devinfinitum0@gmail.com>
2021-03-20 13:32:14 +0500

    Fix the bugs

--- index.js
+++ index.js
@@ -24,12 +24,16 @@ app.get("/login", async (_, res) => {
     res.render("login")
 })
 
+app.get("/signup", async (_, res) => {
+
+    res.render("signup")
+})
+
 app.get("/admin", (_, res) => {
 
     const data = localStorage.getItem("admin.json");
-    const parsed = JSON.parse(data);
 
-    if (parsed) {
+    if (data) {
         res.render("admin", {
             user: JSON.parse(data),
         });
@@ -44,29 +48,54 @@ app.post("/admin", async (req, res) => {
     if (req.body.logout === '') {
         localStorage.removeItem("admin.json");
         res.redirect("/")
-        console.log('ok');
     } else {
         console.log('not found');
     }
 })
 
-app.post("/login", async (req, res) => {
+app.post("/signup", async (req, res) => {
 
     const admin = await fs.readFile("./data/data.json", "utf8");
     const parsed = JSON.parse(admin);
 
+    parsed.unshift({
+        name: req.body.name,
+        userName: req.body.username,
+        role: req.body.role,
+        password: req.body.password
+    }) 
+
+    res.redirect("/")
+
+    // const foundUser = parsed.find(user => user.userName === req.body.username)
+    
+    // if (req.body.role === "admin") {
+        //     res.redirect("/")
+        // } else {
+            //     res.redirect("/");
+            // }
+
+            await fs.writeFile("./data/data.json", JSON.stringify(parsed, null, 4));
+        })
+        
+app.post("/login", async (req, res) => {
+    
+    const admin = await fs.readFile("./data/data.json", "utf8");
+    const parsed = JSON.parse(admin);
+    const usersData = [];
+    
     const found = parsed.find(user => user.userName === req.body.username && user.password === req.body.password);
 
+    usersData.unshift(found)
+    
     if (found) {
-
-        localStorage.setItem("admin.json", JSON.stringify(found, null, 4));
+        
+        localStorage.setItem("admin.json", JSON.stringify(usersData, null, 4));
         res.redirect("/admin");
-
     } else {
         res.redirect("/login");
     }
 })
-
 
 const PORT = process.env.PORT || 2001;
 
